56cf0617c27cf73f9267fd7bcfc28184db9ccb7d
Hamzeh <hamzeh.naser06@gmail.com>
2026-09-17 17:26:38 +0200

    Testa studenter och kurser

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Course programmering = new Course("Programmering", 2);
+
+Course matematik = new Course("Matematik", 3);
+
+Student hamzeh = new Student("Hamzeh");
+
+Student yasmin = new Student("Yasmin");
+
+Student adam = new Student("Adam");
+
+hamzeh.Join(programmering);
+
+yasmin.Join(programmering);
+
+adam.Join(matematik);
+
+Console.WriteLine(programmering);
+programmering.RollCall();
+
+Console.WriteLine();
+
+Console.WriteLine(hamzeh);
+hamzeh.Schedule();
+
+Console.WriteLine("\nEfter att Hamzeh lämnar:");
+
+hamzeh.Leave(programmering);
+
+Console.WriteLine(programmering);
+programmering.RollCall();
+
+Console.WriteLine("\nHamzehs kurser:");
+hamzeh.Schedule();
