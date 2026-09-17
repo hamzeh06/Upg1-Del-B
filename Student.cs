d64769b39cdfe830513325a56f33920ce9364fc9
Hamzeh <hamzeh.naser06@gmail.com>
2026-09-17 17:16:40 +0200

    Skapa Student klassen

--- Student.cs
+++ Student.cs
@@ -6,4 +6,27 @@ class Student
     {
         Name = name;
     }
+    
+    public void Join(Course course)
+    {
+        course.Enroll(this);
+    }
+
+    public void Leave(Course course)
+    {
+        course.Remove(this);
+    }
+
+    public void Schedule()
+    {
+        foreach (Course course in Courses)
+        {
+            Console.WriteLine(course.Name);
+        }
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
 }
